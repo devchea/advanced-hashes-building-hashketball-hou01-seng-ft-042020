e9d3487942c86bce4cbaf277254208ed685239cc
Richard Chea <rchea817@gmail.com>
2020-04-06 06:17:55 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,9 +2,19 @@
 def game_hash()
   new_hash = {
     :home => {
-      :team_name => ""
-      :colors
-      :players
+      :team_name => "",
+      :colors => [],
+      :players => [
+        :player_name
+        :number
+        :shoe
+        :points
+        :rebounds
+        :assists
+        :steals
+        :blocks
+        :slam_dunks
+      ]
     },
     :away => {
       :team_name
